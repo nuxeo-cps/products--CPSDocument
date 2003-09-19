315e403645520d4af179b3eb54ed059427c13977
hcauwelier <devnull@localhost>
2003-09-19 12:59:27 +0000

    fix the portal types' dictionnary format to what the CPSDocument install script excepts and gives to set the portal type object's properties. I don't understand how it could worked... Note that it also affects your getCustomDocumentTypes script.

--- skins/cps_document/getDocumentTypes.py
+++ skins/cps_document/getDocumentTypes.py
@@ -5,37 +5,36 @@
 Here are defined list of portal type created with CPSDocument
 """
 faq_type = {
-    'FAQ': {'title': 'portal_type_FAQ_title',
-            'description': 'portal_type_FAQ_description',
-            'content_icon': 'faq_icon.gif',
-            'immediate_view': 'cpsdocument_edit_form',
-            'schemas': ['faq'],
-            'default_layout': 'faq',
-            'layout_style_prefix': 'layout_dummy_',
-            'cps_proxy_type': 'document',
-             },
-        }
+    'title': 'portal_type_FAQ_title',
+    'description': 'portal_type_FAQ_description',
+    'content_icon': 'faq_icon.gif',
+    'immediate_view': 'cpsdocument_edit_form',
+    'schemas': ['faq'],
+    'default_layout': 'faq',
+    'layout_style_prefix': 'layout_dummy_',
+    'cps_proxy_type': 'document',
+     }
+
 news_type = {
-    'News': {'title': 'portal_type_News_title',
-             'description': 'portal_type_News_description',
-             'content_icon': 'news_icon.gif',
-             'immediate_view': 'cpsdocument_edit_form',
-             'schemas': ['news'],
-             'default_layout': 'news',
-             'layout_style_prefix': 'layout_default_',
-             'cps_proxy_type': 'document',
-             },
-        }
+    'title': 'portal_type_News_title',
+    'description': 'portal_type_News_description',
+    'content_icon': 'news_icon.gif',
+    'immediate_view': 'cpsdocument_edit_form',
+    'schemas': ['news'],
+    'default_layout': 'news',
+    'layout_style_prefix': 'layout_default_',
+    'cps_proxy_type': 'document',
+    }
+
 dummy_form_type = {
-    'Dummy Form': {'title': 'portal_type_Dummy_Form',
-                   'description': 'portal_type_Dummy_description',
-                   'content_icon': 'document_icon.gif',
-                   'immediate_view': 'cpsdocument_edit_form',
-                   'schemas': ['dummy_form'],
-                   'default_layout': 'dummy_form',
-                   'layout_style_prefix': 'layout_form_',
-                   'cps_proxy_type': 'document',
-                   },
+    'title': 'portal_type_Dummy_Form',
+    'description': 'portal_type_Dummy_description',
+    'content_icon': 'document_icon.gif',
+    'immediate_view': 'cpsdocument_edit_form',
+    'schemas': ['dummy_form'],
+    'default_layout': 'dummy_form',
+    'layout_style_prefix': 'layout_form_',
+    'cps_proxy_type': 'document',
     }
 
 types = {}
