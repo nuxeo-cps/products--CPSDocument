15a94312266183010865449016d5343446393071
edegoute <devnull@localhost>
2003-09-22 12:39:59 +0000

    add breve and fichier schemas

--- skins/cps_document/getDocumentSchemas.py
+++ skins/cps_document/getDocumentSchemas.py
@@ -10,94 +10,226 @@ Please, follow the same pattern to add new schemas.
 #########################################################
 # FAQ SHEMA
 #########################################################
-faq_schema = { 'title': {'type': 'CPS String Field',
-                         'data': {'default': '',
-                                  'is_indexed': 1,
-                                  },
-                         },
-               'description': {'type': 'CPS String Field',
-                               'data': {'default': '',
-                                        'is_indexed': 1,
-                                        },
-                               },
-               'question': {'type': 'CPS String Field',
-                            'data': {'default': '',
-                                     'is_indexed': 0,
-                                     },
-                            },
-               'answer': {'type': 'CPS String Field',
-                          'data': {'default': '',
-                                   'is_indexed': 0,
-                                   },
-                          },
-               }
+
+faq_schema = {
+    'title': {
+        'type': 'CPS String Field',
+        'data': {
+                'default': '',
+                'allow_none': 0,
+                'is_indexed': 1,
+            },
+        },
+    'description': {
+        'type': 'CPS String Field',
+        'data': {
+                'default': '',
+                'allow_none': 0,
+                'is_indexed': 1,
+            },
+        },
+    'question': {
+        'type': 'CPS String Field',
+        'data': {
+                'default': '',
+                'allow_none': 0,
+                'is_indexed': 0,
+            },
+        },
+    'answer': {
+        'type': 'CPS String Field',
+        'data': {
+                'default': '',
+                'allow_none': 0,
+                'is_indexed': 0,
+            },
+        },
+    }
 
 ########################################################
 # DUMMY FORM SCHEMA
 ########################################################
 
-dummy_form_schema = {'title': {'type': 'CPS String Field',
-                               'data': {'default': '',
-                                        'is_indexed': 1,
-                                        },
-                               },
-                     'description': {'type': 'CPS String Field',
-                                     'data': {'default': '',
-                                              'is_indexed': 1,
-                                              },
-                                     },
-                     }
+dummy_form_schema = {
+    'title': {
+        'type': 'CPS String Field',
+        'data': {
+                'default': '',
+                'allow_none': 0,
+                'is_indexed': 1,
+            },
+        },
+    'description': {
+        'type': 'CPS String Field',
+        'data': {
+                'default': '',
+                'allow_none': 0,
+                'is_indexed': 1,
+            },
+        },
+    }
 
 ########################################################
-# NEWS SCHEMA
+# News SCHEMA
 ########################################################
-news_schema = {'title': { 'type': 'CPS String Field',
-                          'data': {'default': '',
-                                   'is_indexed': 1,
-                                   },
-                          },
-               'description': {'type': 'CPS String Field',
-                               'data': {'default': 'resume',
-                                        'is_indexed': 1,
-                                        },
-                               },
-               'theme': {'type': 'CPS String List Field',
-                               'data': {'default': '',
-                                        'is_indexed': 1,
-                                        },
-                               },
-               'longTitle': {'type': 'CPS String Field',
-                             'data': {'default': '',
-                                      'is_indexed': 1,
-                                      },
-                             },
-               'newsdate': {'type': 'CPS DateTime Field',
-                            'data': {'default': '',
-                                     'is_indexed': 0,
-                                     },
-                            },
-               'content': {'type': 'CPS String Field',
-                           'data': {'default': '',
-                                    'is_indexed': 1,
-                                    },
-                           },
-               'photo': {'type': 'CPS Image Field',
-                         'data': {'default': '',
-                                  'is_indexed': 0,
-                                  },
-                         },
-               'preview': {'type': 'CPS Image Field',
-                           'data': {'default': '',
-                                    'is_indexed': 0,
-                                    },
-                           },
-               'attachedFile': {'type': 'CPS File Field',
-                           'data': {'default': '',
-                                    'is_indexed': 0,
-                                    },
-                           },
-               }
 
+news_schema = {
+    'preview': {
+        'type': 'CPS Image Field',
+        'data': {
+                'default': '',
+                'allow_none': 0,
+                'is_indexed': 0,
+            },
+        },
+    'attachedFile': {
+        'type': 'CPS File Field',
+        'data': {
+                'default': '',
+                'allow_none': 0,
+                'is_indexed': 0,
+                'suffix_html': '',
+                'suffix_text': '',
+            },
+        },
+    'newsdate': {
+        'type': 'CPS DateTime Field',
+        'data': {
+                'default': '',
+                'allow_none': 1,
+                'is_indexed': 0,
+            },
+        },
+    'title': {
+        'type': 'CPS String Field',
+        'data': {
+                'default': '',
+                'allow_none': 0,
+                'is_indexed': 1,
+            },
+        },
+    'photo': {
+        'type': 'CPS Image Field',
+        'data': {
+                'default': '',
+                'allow_none': 0,
+                'is_indexed': 0,
+            },
+        },
+    'description': {
+        'type': 'CPS String Field',
+        'data': {
+                'default': 'resume',
+                'allow_none': 0,
+                'is_indexed': 1,
+            },
+        },
+    'theme': {
+        'type': 'CPS String List Field',
+        'data': {
+                'default': '',
+                'allow_none': 0,
+                'is_indexed': 1,
+            },
+        },
+    'longTitle': {
+        'type': 'CPS String Field',
+        'data': {
+                'default': '',
+                'allow_none': 0,
+                'is_indexed': 1,
+            },
+        },
+    'content': {
+        'type': 'CPS String Field',
+        'data': {
+                'default': '',
+                'allow_none': 0,
+                'is_indexed': 1,
+            },
+        },
+    }
+
+########################################################
+# Breve SCHEMA
+########################################################
+
+breve_schema = {
+    'title': {
+        'type': 'CPS String Field',
+        'data': {
+                'default': '',
+                'allow_none': 0,
+                'is_indexed': 1,
+            },
+        },
+    'description': {
+        'type': 'CPS String Field',
+        'data': {
+                'default': '',
+                'allow_none': 0,
+                'is_indexed': 1,
+            },
+        },
+    'date': {
+        'type': 'CPS DateTime Field',
+        'data': {
+                'default': '',
+                'allow_none': 1,
+                'is_indexed': 0,
+            },
+        },
+    'image': {
+        'type': 'CPS Image Field',
+        'data': {
+                'default': '',
+                'allow_none': 0,
+                'is_indexed': 0,
+            },
+        },
+    'body': {
+        'type': 'CPS String Field',
+        'data': {
+                'default': '',
+                'allow_none': 0,
+                'is_indexed': 1,
+            },
+        },
+    }
+
+########################################################
+# Breve SCHEMA
+########################################################
+
+fichier_schema = {
+    'title': {
+        'type': 'CPS String Field',
+        'data': {
+                'default': '',
+                'allow_none': 0,
+                'is_indexed': 0,
+            },
+        },
+    'description': {
+        'type': 'CPS String Field',
+        'data': {
+                'default': '',
+                'allow_none': 0,
+                'is_indexed': 0,
+            },
+        },
+    'file': {
+        'type': 'CPS File Field',
+        'data': {
+                'default': '',
+                'allow_none': 0,
+                'is_indexed': 1,
+                'suffix_html': '.html',
+                'suffix_text': '.txt',
+            },
+        },
+    }
+    
 ###########################################################
 # END OF SCHEMAS DEFINITIONS
 ###########################################################
@@ -111,6 +243,8 @@ schemas = {}
 schemas['faq'] = faq_schema
 schemas['dummy_form'] = dummy_form_schema
 schemas['news'] = news_schema
+schemas['breve'] = breve_schema
+schemas['fichier'] = fichier_schema
 
 cschemas = context.getCustomDocumentSchemas()
 
