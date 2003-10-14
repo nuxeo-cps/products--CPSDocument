a35f2785449f3d86fc6372b1cb956727e44d63a8
fguillaume <devnull@localhost>
2003-10-14 12:14:25 +0000

    Redirect to immediate_view (Initial view name) after creation. Use an initial view name of cpsdocument_view for cps document types, now that all validation is done at creation time.

--- skins/cps_document/cpsdocument_created.py
+++ skins/cps_document/cpsdocument_created.py
@@ -10,7 +10,6 @@ May return a rendered document, or do a redirect.
 # $Id$
 
 psm = 'psm_content_created'
-#action_path = context.getTypeInfo().immediate_view
-action_path = ''
+action_path = context.getTypeInfo().immediate_view
 context.REQUEST.RESPONSE.redirect('%s/%s?portal_status_message=%s' %
                                   (context.absolute_url(), action_path, psm))
