cbed1a9e62e0cdae4dfa9bcaf59a2750426bb722
epietrig <devnull@localhost>
2003-11-05 09:50:20 +0000

    filters a list of proxies (criteria: title starts by..., case insensitive)

---
+++ skins/cps_document/getTitlesStartingBy.py
@@ -0,0 +1,14 @@
+##parameters=proxydocs,strt=None,all=0
+# $Id $
+
+res = []
+
+if strt:
+    str1 = strt.lower()
+    for proxy in proxydocs:
+        if proxy.getContent()['title'].lower().startswith(strt):
+            res.append(proxy)
+elif all:
+    res=proxydocs
+#return empty list if no letter specified (and all is false) 
+return res
