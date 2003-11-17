5b53644e87da9b39060d1f1fc5792e8d3243d192
sfermigier <devnull@localhost>
2003-11-17 17:23:00 +0000

    Refactor some tests (use API instead of hacking though attributes) + add an interface test (we need more of these !!!).

--- tests/testDefaultDocuments.py
+++ tests/testDefaultDocuments.py
@@ -11,36 +11,52 @@ import unittest
 from Testing import ZopeTestCase
 import CPSDocumentTestCase
 
+
 class TestDocuments(CPSDocumentTestCase.CPSDocumentTestCase):
     def afterSetUp(self):
         self.login('root')
         self.ws = self.portal.workspaces
-        self.schemas = self.portal.getDocumentSchemas()
-        self.types = self.portal.getDocumentTypes()
+        self.document_schemas = self.portal.getDocumentSchemas()
+        self.document_types = self.portal.getDocumentTypes()
 
     def beforeTearDown(self):
         self.logout()
 
     def testCreateDocumentsInWorkspacesRoot(self):
-        for doc_type in self.types.keys():
+        for doc_type in self.document_types.keys():
             doc_id = doc_type.lower()
             self.ws.invokeFactory(doc_type, doc_id)
             self._testDefaultAttributes(doc_id)
+            self._testInterfaces(doc_id)
 
     def _testDefaultAttributes(self, doc_id):
-        doc = getattr(self.portal.workspaces, doc_id)
+        doc = getattr(self.ws, doc_id)
         # Edit doc to set default values
         doc = doc.getContent()
         doc.edit()
 
-        # Retrieve doc schema and test if attributes have been created
-        doc_type = doc.portal_type
-        tt = self.portal.portal_types
-        fti = tt[doc_type]
+        type_info = doc.getTypeInfo()
 
-        for schema in fti.schemas:
-            for prop_name in self.schemas[schema].keys():
+        for schema in type_info.schemas:
+            for prop_name in self.document_schemas[schema].keys():
                 self.assert_(hasattr(doc, prop_name))
+
+    def _testInterfaces(self, doc_id):
+        from Interface.Verify import verifyClass
+        from Products.CMFCore.interfaces.Dynamic \
+            import DynamicType as IDynamicType
+        from Products.CMFCore.interfaces.Contentish \
+            import Contentish as IContentish
+        from Products.CMFCore.interfaces.DublinCore \
+            import DublinCore as IDublinCore
+
+        doc = getattr(self.ws, doc_id).getContent()
+        verifyClass(IDynamicType, doc.__class__)
+        verifyClass(IContentish, doc.__class__)
+        # XXX: CPSDocument inherits from DefaultDublinCoreImpl
+        # so this test should pass -> ???
+        #verifyClass(IDublinCore, doc.__class__)
+
 
     # XXX: this should work by fixing ZTC (add some methode to the
     # fake RESPONSE object)
@@ -58,7 +74,7 @@ class TestDocuments(CPSDocumentTestCase.CPSDocumentTestCase):
         doc.edit()
 
         # Test doc has default values
-        for prop_name in self.schemas['news'].keys():
+        for prop_name in self.document_schemas['news'].keys():
             # XXX: Default values are not always as defined in
             # getDocumentSchemas(). I consider this as a bug.
             self.assert_(hasattr(doc, prop_name))
