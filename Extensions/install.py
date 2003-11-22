8051b1f866c728a4ef59205e691b04e8703d91b9
sfermigier <devnull@localhost>
2003-11-22 20:28:19 +0000

    Major refactoring of installer.

--- Extensions/install.py
+++ Extensions/install.py
@@ -35,183 +35,231 @@ from App.Extensions import getPath
 from re import match
 from zLOG import LOG, INFO, DEBUG
 
-def install(self):
+SECTIONS_ID = 'sections'
+WORKSPACES_ID = 'workspaces'
 
-    _log = []
-    def pr(bla, zlog=1, _log=_log):
-        if bla == 'flush':
-            return '<html><head><title>CPSDocument Update</title></head><body><pre>'+ \
-                   '\n'.join(_log) + \
-                   '</pre></body></html>'
+class Installer:
+    def __init__(self, context):
+        self._log = []
+        self.context = context
+        self.portal = context.portal_url.getPortalObject()
 
-        _log.append(bla)
+
+    def install(self):
+        self.log("Starting CPSDocument install")
+        self.setupSkins()
+        self.installCPSSchemas()
+        self.setupPortalTypes()
+        self.setupTranslations()
+        self.log("End of specific CPSDocument install")
+
+    #
+    # Logging
+    #
+    def log(self, bla, zlog=1):
+        self._log.append(bla)
         if (bla and zlog):
             LOG('CPSDocument install:', INFO, bla)
 
-    def prok(pr=pr):
-        pr(" Already correctly installed")
+    def logOK(self):
+        self.log(" Already correctly installed")
 
-    pr("Starting CPSDocument install")
-
-    portal = self.portal_url.getPortalObject()
-
-    def portalhas(id, portal=portal):
-        return id in portal.objectIds()
+    def logResult(self):
+        return '<html><head><title>CPSDocument Update</title></head>' \
+            '<body><pre>'+ '\n'.join(self._log) + '</pre></body></html>'
 
 
-    # skins
-    skins = ('cps_document',)
-    paths = {
-        'cps_document': 'Products/CPSDocument/skins/cps_document',
-    }
-    skin_installed = 0
-    for skin in skins:
-        path = paths[skin]
-        path = path.replace('/', os.sep)
-        pr(" FS Directory View '%s'" % skin)
-        if skin in portal.portal_skins.objectIds():
-            dv = portal.portal_skins[skin]
-            oldpath = dv.getDirPath()
-            if oldpath == path:
-                prok()
-            else:
-                pr("  Correctly installed, correcting path")
-                dv.manage_properties(dirpath=path)
-        else:
-            skin_installed = 1
-            portal.portal_skins.manage_addProduct['CMFCore'].manage_addDirectoryView(filepath=path, id=skin)
-            pr("  Creating skin")
-    if skin_installed:
-        allskins = portal.portal_skins.getSkinPaths()
-        for skin_name, skin_path in allskins:
-            if skin_name != 'Basic':
-                continue
-            path = [x.strip() for x in skin_path.split(',')]
-            path = [x for x in path if x not in skins] # strip all
-            if path and path[0] == 'custom':
-                path = path[:1] + list(skins) + path[1:]
-            else:
-                path = list(skins) + path
-            npath = ', '.join(path)
-            portal.portal_skins.addSkinSelection(skin_name, npath)
-            pr(" Fixup of skin %s" % skin_name)
-        pr(" Resetting skin cache")
-        portal._v_skindata = None
-        portal.setupCurrentSkin()
-
-    # call cpsschemas install
-    from Products.CPSSchemas.Extensions.install import install as \
-         cpschemas_install
-    res = cpschemas_install(self)
-    pr(res)
-
-    # setup portal_type
-    pr("Verifying portal types")
-    flextypes = self.getDocumentTypes()
-    newptypes = flextypes.keys()
-    ttool = portal.portal_types
-    if 'Workspace' in ttool.objectIds():
-        workspaceACT = list(ttool['Workspace'].allowed_content_types)
-    else:
-        raise "DependanceError", 'Workspace'
-    for ptype in newptypes:
-        if ptype not in  workspaceACT:
-            workspaceACT.append(ptype)
-
-    allowed_content_type = {
-                            'Workspace' : workspaceACT,
-                            }
-
-    ttool['Workspace'].allowed_content_types = allowed_content_type['Workspace']
-
-    ptypes_installed = ttool.objectIds()
-    display_in_cmf_calendar = []
-    for ptype, data in flextypes.items():
-        pr("  Type '%s'" % ptype)
-        if ptype in ptypes_installed:
-            ttool.manage_delObjects([ptype])
-            pr("   Deleted")
-        ti = ttool.addFlexibleTypeInformation(id=ptype)
-        if data.get('display_in_cmf_calendar'):
-            display_in_cmf_calendar.append(ptype)
-            del data['display_in_cmf_calendar']
-        ti.manage_changeProperties(**data)
-        pr("   Installation")
-
-    # register ptypes to portal_calendar
-    if len(display_in_cmf_calendar):
-        portal.portal_calendar.calendar_types = display_in_cmf_calendar
-
-    # check site and workspaces proxies
-    sections_id = 'sections'
-    workspaces_id = 'workspaces'
-
-    # register folderish document types in portal_tree
-    pr("Registering folderish document types in portal_tree")
-    trtool = portal.portal_trees
-    trtool[workspaces_id].manage_changeProperties(
-        type_names=list(trtool[workspaces_id].type_names) + ['FAQ', 'ImageGallery', 'Glossary']
-        )
-    trtool[workspaces_id].manage_rebuild()
-    trtool[sections_id].manage_changeProperties(
-        type_names=list(trtool[sections_id].type_names) + ['FAQ', 'ImageGallery', 'Glossary']
-        )
-    trtool[sections_id].manage_rebuild()
-
-    # check workflow association
-    pr("Verifying local workflow association")
-    if not '.cps_workflow_configuration' in portal[workspaces_id].objectIds():
-        raise "DependanceError", 'no .cps_workflow_configuration in Workspace'
-    else:
-        wfc = getattr(portal[workspaces_id], '.cps_workflow_configuration')
-
-    for ptype in newptypes:
-        if 'cps_workspace_wf' in flextypes[ptype].keys():
-            wwf = flextypes[ptype]['cps_workspace_wf']
-        else:
-            wwf = 'workspace_content_wf'
-        pr("  Add %s chain to portal type %s in %s of %s" %(wwf,
-             ptype, '.cps_workflow_configuration', workspaces_id))
-        wfc.manage_addChain(portal_type=ptype, chain=wwf)
-
-    if not '.cps_workflow_configuration' in portal[sections_id].objectIds():
-        raise "DependanceError", 'no .cps_workflow_configuration in Section'
-    else:
-        wfc = getattr(portal[sections_id], '.cps_workflow_configuration')
-
-    for ptype in newptypes:
-        pr("  Add %s chain to portal type %s in %s of %s" %('section_content_wf',
-             ptype, '.cps_workflow_configuration', sections_id))
-        wfc.manage_addChain(portal_type=ptype,
-                            chain='section_content_wf')
-
-    # importing .po files
-    mcat = portal['Localizer']['default']
-    pr(" Checking available languages")
-    podir = os.path.join('Products', 'CPSDocument')
-    popath = getPath(podir, 'i18n')
-    if popath is None:
-        pr(" !!! Unable to find .po dir")
-    else:
-        pr("  Checking installable languages")
-        langs = []
-        avail_langs = mcat.get_languages()
-        pr("    Available languages: %s" % str(avail_langs))
-        for file in os.listdir(popath):
-            if file.endswith('.po'):
-                m = match('^.*([a-z][a-z])\.po$', file)
-                if m is None:
-                    pr( '    Skipping bad file %s' % file)
-                    continue
-                lang = m.group(1)
-                if lang in avail_langs:
-                    lang_po_path = os.path.join(popath, file)
-                    lang_file = open(lang_po_path)
-                    pr("    Importing %s into '%s' locale" % (file, lang))
-                    mcat.manage_import(lang, lang_file)
+    #
+    # These methods do the actual work
+    #
+    # TODO: this is almost completely generic code, it should move to 
+    # a superclass
+    def setupSkins(self):
+        # skins
+        skins = ('cps_document',)
+        paths = {
+            'cps_document': 'Products/CPSDocument/skins/cps_document',
+        }
+        skin_installed = 0
+        for skin in skins:
+            path = paths[skin]
+            path = path.replace('/', os.sep)
+            self.log(" FS Directory View '%s'" % skin)
+            if skin in self.portal.portal_skins.objectIds():
+                dv = self.portal.portal_skins[skin]
+                oldpath = dv.getDirPath()
+                if oldpath == path:
+                    self.logok()
                 else:
-                    pr( '    Skipping not installed locale for file %s' % file)
+                    self.log("  Correctly installed, correcting path")
+                    dv.manage_properties(dirpath=path)
+            else:
+                skin_installed = 1
+                self.portal.portal_skins.manage_addProduct['CMFCore'].manage_addDirectoryView(filepath=path, id=skin)
+                self.log("  Creating skin")
+        if skin_installed:
+            allskins = self.portal.portal_skins.getSkinPaths()
+            for skin_name, skin_path in allskins:
+                if skin_name != 'Basic':
+                    continue
+                path = [x.strip() for x in skin_path.split(',')]
+                path = [x for x in path if x not in skins] # strip all
+                if path and path[0] == 'custom':
+                    path = path[:1] + list(skins) + path[1:]
+                else:
+                    path = list(skins) + path
+                npath = ', '.join(path)
+                self.portal.portal_skins.addSkinSelection(skin_name, npath)
+                self.log(" Fixup of skin %s" % skin_name)
+            self.log(" Resetting skin cache")
+            self.portal._v_skindata = None
+            self.portal.setupCurrentSkin()
+
+    def installCPSSchemas(self):
+        # call cpsschemas install
+        from Products.CPSSchemas.Extensions.install import install as \
+             cpschemas_install
+        res = cpschemas_install(self.context)
+        self.log(res)
+
+    def setupPortalTypes(self):
+        # setup portal_types
+        self.log("Verifying portal types")
+        self.flextypes = self.portal.getDocumentTypes()
+        self.newptypes = self.flextypes.keys()
+        self.ttool = self.portal.portal_types
+
+        self.allowTypesInWorkspaces()
+        self.registerTypes()
+        self.updatePortalTree()
+        self.updateWorkflowAssociations()
+
+    def allowTypesInWorkspaces(self):
+        if 'Workspace' in self.ttool.objectIds():
+            workspace_act = list(self.ttool['Workspace'].allowed_content_types)
+        else:
+            raise "DependanceError", 'Workspace'
+        for ptype in self.newptypes:
+            if ptype not in workspace_act:
+                workspace_act.append(ptype)
+        self.ttool['Workspace'].allowed_content_types = workspace_act
+
+    def registerTypes(self):
+        ptypes_installed = self.ttool.objectIds()
+        display_in_cmf_calendar = []
+        for ptype, data in self.flextypes.items():
+            self.log("  Type '%s'" % ptype)
+            if ptype in ptypes_installed:
+                self.ttool.manage_delObjects([ptype])
+                self.log("   Deleted")
+            ti = self.ttool.addFlexibleTypeInformation(id=ptype)
+            if data.get('display_in_cmf_calendar'):
+                display_in_cmf_calendar.append(ptype)
+                del data['display_in_cmf_calendar']
+            ti.manage_changeProperties(**data)
+            self.log("   Installation")
+
+        # register ptypes to portal_calendar
+        if display_in_cmf_calendar:
+            self.portal.portal_calendar.calendar_types = display_in_cmf_calendar
+
+    def updateWorkflowAssociations(self):
+        # check workflow association
+        self.log("Verifying local workflow association")
+
+        sections = self.portal[SECTIONS_ID]
+        workspaces = self.portal[WORKSPACES_ID]
+
+        if not '.cps_workflow_configuration' in workspaces.objectIds():
+            raise "DependanceError", 'no .cps_workflow_configuration in Workspace'
+        else:
+            wfc = getattr(workspaces, '.cps_workflow_configuration')
+
+        for ptype in self.newptypes:
+            if 'cps_workspace_wf' in self.flextypes[ptype].keys():
+                wwf = self.flextypes[ptype]['cps_workspace_wf']
+            else:
+                wwf = 'workspace_content_wf'
+            self.log("  Add %s chain to portal type %s in %s of %s" % (
+                wwf, ptype, '.cps_workflow_configuration', WORKSPACES_ID))
+            wfc.manage_addChain(portal_type=ptype, chain=wwf)
+
+        if not '.cps_workflow_configuration' in sections.objectIds():
+            raise "DependanceError", 'no .cps_workflow_configuration in Section'
+        else:
+            wfc = getattr(sections, '.cps_workflow_configuration')
+
+        for ptype in self.newptypes:
+            self.log("  Add %s chain to portal type %s in %s of %s" % (
+                'section_content_wf', ptype, '.cps_workflow_configuration',
+                SECTIONS_ID))
+            wfc.manage_addChain(portal_type=ptype,
+                                chain='section_content_wf')
+
+    def updatePortalTree(self):
+        # register folderish document types in portal_tree
+        self.log("Registering folderish document types in portal_tree")
+        trtool = self.portal.portal_trees
+        trtool[WORKSPACES_ID].manage_changeProperties(
+            type_names=list(trtool[WORKSPACES_ID].type_names) 
+                + ['FAQ', 'ImageGallery', 'Glossary'])
+        trtool[WORKSPACES_ID].manage_rebuild()
+        trtool[SECTIONS_ID].manage_changeProperties(
+            type_names=list(trtool[SECTIONS_ID].type_names) 
+                + ['FAQ', 'ImageGallery', 'Glossary'])
+        trtool[SECTIONS_ID].manage_rebuild()
+
+    # TODO: this method is completely generic and should go to a superclass
+    def setupTranslations(self):
+        """Import .po files"""
+
+        mcat = self.portal['Localizer']['default']
+        self.log(" Checking available languages")
+        podir = os.path.join('Products', 'CPSDocument')
+        popath = getPath(podir, 'i18n')
+        if popath is None:
+            self.log(" !!! Unable to find .po dir")
+        else:
+            self.log("  Checking installable languages")
+            langs = []
+            avail_langs = mcat.get_languages()
+            self.log("    Available languages: %s" % str(avail_langs))
+            for file in os.listdir(popath):
+                if file.endswith('.po'):
+                    m = match('^.*([a-z][a-z])\.po$', file)
+                    if m is None:
+                        self.log('    Skipping bad file %s' % file)
+                        continue
+                    lang = m.group(1)
+                    if lang in avail_langs:
+                        lang_po_path = os.path.join(popath, file)
+                        lang_file = open(lang_po_path)
+                        self.log("    Importing %s into '%s' locale" % (file, lang))
+                        mcat.manage_import(lang, lang_file)
+                    else:
+                        self.log('    Skipping not installed locale for file %s' % file)
+
+class CMFInstaller(Installer):
+    def setupPortalTypes(self):
+        # setup portal_types
+        self.log("Verifying portal types")
+        self.flextypes = self.portal.getDocumentTypes()
+        self.newptypes = self.flextypes.keys()
+        self.ttool = self.portal.portal_types
+
+        #self.allowTypesInWorkspaces()
+        self.registerTypes()
+        #self.updatePortalTree()
+        #self.updateWorkflowAssociations()
 
 
-    pr("End of specific CPSDocument install")
-    return pr('flush')
+def install(self):
+    installer = Installer(self)
+    installer.install()
+    return installer.logResult()
+
+def cmfinstall(self):
+    installer = Installer(self)
+    installer.install()
+    return installer.logResult()
+
