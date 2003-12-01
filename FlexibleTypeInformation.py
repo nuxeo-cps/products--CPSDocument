5a5ca65d2c72cf0c3d712b35fb2cc70f75c141ac
fguillaume <devnull@localhost>
2003-12-01 21:35:48 +0000

    Unused import (found by pychecker2).

--- FlexibleTypeInformation.py
+++ FlexibleTypeInformation.py
@@ -30,7 +30,6 @@ from Products.CMFCore.CMFCorePermissions import View
 from Products.CMFCore.CMFCorePermissions import ModifyPortalContent
 from Products.CMFCore.CMFCorePermissions import ChangePermissions
 from Products.CMFCore.CMFCorePermissions import ManagePortal
-from Products.CMFCore.utils import _checkPermission
 from Products.CMFCore.utils import getToolByName
 from Products.CMFCore.TypesTool import FactoryTypeInformation
 from Products.CMFCore.interfaces.portal_types \
