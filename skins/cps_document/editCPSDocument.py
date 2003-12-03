3839d8c89d1dd860a44f2fe64e8ee11c1ba87b11
bdelbosc <devnull@localhost>
2003-12-03 12:05:51 +0000

    creation

---
+++ skins/cps_document/editCPSDocument.py
@@ -0,0 +1,25 @@
+##parameters=REQUEST, layout_id=None
+# $Id$
+"""
+edit layout and content if form submited
+
+return html renderer + psm
+"""
+doc = context.getContent()
+
+layout_changed = context.editLayouts(REQUEST=REQUEST);
+
+if layout_changed or REQUEST.has_key('cpsdocument_edit_button'):
+    request = REQUEST
+    psm = 'psm_content_changed'
+else:
+    request = None
+    psm = ''
+
+res = doc.renderEditDetailed(request=request, proxy=context,
+                             layout_id=layout_id)
+
+if not res[1]:
+    psm = 'psm_content_error'
+
+return res[0], psm
