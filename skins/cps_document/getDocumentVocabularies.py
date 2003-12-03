e45bd562033b535fd163c5f5058c820bdf2fada5
bdelbosc <devnull@localhost>
2003-12-03 16:49:28 +0000

    adding subject_voc default

--- skins/cps_document/getDocumentVocabularies.py
+++ skins/cps_document/getDocumentVocabularies.py
@@ -5,7 +5,21 @@ Here are defined the vocabularies.
 Please, follow the same pattern to add new ones.
 """
 
+def make_dict(voc):
+    d = {}
+    l = []
+    for v in voc:
+        d[v] = v
+        l.append(v)
+    return {'data': {'dict': d, 'list': l}}
+
+subject_voc = ("Arts", "Business", "Computers", "Games", "Health",
+               "Home", "Kids and Teens", "News", "Recreation",
+               "Reference", "Regional", "Science", "Shopping",
+               "Society", "Sports")
+
 vocabularies = {
+    'subject_voc': make_dict(subject_voc),
     'dummy_voc': {
         'type': 'CPS Vocabulary',
         'data': {
