50e78fa29b06897a7d394d0770bf53f6e6223755
bdelbosc <devnull@localhost>
2003-12-04 09:19:48 +0000

    remove date theme from common do not write Modification, creation dates and creator, format and language

--- skins/cps_document/getDocumentSchemas.py
+++ skins/cps_document/getDocumentSchemas.py
@@ -11,38 +11,33 @@ Please, follow the same pattern to add new schemas.
 #########################################################
 #metadata schema
 metadata_schema = {
-    'Title': {'type': 'CPS String Field', 'data': {'is_indexed': 1,}},
-    'Description': {'type': 'CPS String Field', 'data': {'is_indexed': 1}},
-    'Subject': {'type': 'CPS String List Field', 'data': {'is_indexed': 1}},
+    'Title': {'type': 'CPS String Field',
+              'data': {'is_indexed': 1,}},
+    'Description': {'type': 'CPS String Field',
+                    'data': {'is_indexed': 1}},
+    'Subject': {'type': 'CPS String List Field',
+                'data': {'is_indexed': 1}},
     'Contributors': {'type': 'CPS String List Field',
                      'data': {'is_indexed': 1}},
-    'CreationDate': {'type': 'CPS DateTime Field', 'data': {}},
-    'ModificationDate': {'type': 'CPS DateTime Field', 'data': {}},
+    'CreationDate': {'type': 'CPS DateTime Field',
+                     'data': {'write_ignore_storage': 1,}},
+    'ModificationDate': {'type': 'CPS DateTime Field',
+                         'data': {'write_ignore_storage': 1,}},
     'EffectiveDate': {'type': 'CPS DateTime Field', 'data': {}},
     'ExpirationDate': {'type': 'CPS DateTime Field', 'data': {}},
-    'Format': {'type': 'CPS String Field', 'data': {}},
-    'Language': {'type': 'CPS String Field', 'data': {}},
+    'Format': {'type': 'CPS String Field',
+               'data': {'write_ignore_storage': 1,}},
+    'Language': {'type': 'CPS String Field',
+                 'data': {'write_ignore_storage': 1,}},
     'Rights': {'type': 'CPS String Field', 'data': {'is_indexed': 1}},
-    'Creator': {'type': 'CPS String Field', 'data': {'is_indexed': 1}},
+    'Creator': {'type': 'CPS String Field',
+                'data': {'is_indexed': 1,
+                         'write_ignore_storage': 1,}},
     }
 
 
 # common schema
 common_schema = {
-    'date': {
-        'type': 'CPS DateTime Field',
-        'data': {
-                'default': '',
-                'is_indexed': 0,
-            },
-        },
-    'theme': {
-        'type': 'CPS String Field',
-        'data': {
-                'default': '',
-                'is_indexed': 1,
-            },
-        },
     'preview': {
         'type': 'CPS Image Field',
         'data': {
