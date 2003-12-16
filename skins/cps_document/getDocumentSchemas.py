48703599b719ed370a6a21b40b0c6c700439e4cc
bdelbosc <devnull@localhost>
2003-12-16 15:46:39 +0000

    80 char by line is better

--- skins/cps_document/getDocumentSchemas.py
+++ skins/cps_document/getDocumentSchemas.py
@@ -20,8 +20,8 @@ metadata_schema = {
     'Contributors': {'type': 'CPS String List Field',
                      'data': {'is_indexed': 1,
                               'write_process_expression_str':
-                              'python: portal.computeContributors(user, value)'},
-                     },
+                              'python: portal.computeContributors(user, value)'
+                              },},
     'CreationDate': {'type': 'CPS DateTime Field',
                      'data': {'write_ignore_storage': 1,}},
     'ModificationDate': {'type': 'CPS DateTime Field',
