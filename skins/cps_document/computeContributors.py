46ff04c1bb7a592aec83b706fbd21a9300af7b50
hcauwelier <devnull@localhost>
2004-01-30 13:58:51 +0000

    Use CPSDirectory instead of NuxMetaDirectories.

--- skins/cps_document/computeContributors.py
+++ skins/cps_document/computeContributors.py
@@ -3,7 +3,7 @@
 """this script is called by the the Contributors write expression
 check if current user is in contributors if not add him"""
 uname = user.getId()
-duser = context.portal_metadirectories['members'].getEntry(uname)
+duser = context.portal_directories['members'].getEntry(uname)
 if duser:
     uname = duser.get('fullname', uname)
 if not value:
