0e9458b53f48066e8b6cc33621b5718d12f6c9cf
fguillaume <devnull@localhost>
2004-02-05 15:55:56 +0000

    Fail correctly if the user has no directory entry.

--- skins/cps_document/computeContributors.py
+++ skins/cps_document/computeContributors.py
@@ -3,7 +3,7 @@
 """this script is called by the the Contributors write expression
 check if current user is in contributors if not add him"""
 uname = user.getId()
-duser = context.portal_directories['members'].getEntry(uname)
+duser = context.portal_directories['members'].getEntry(uname, default=None)
 if duser:
     uname = duser.get('fullname', uname)
 if not value:
