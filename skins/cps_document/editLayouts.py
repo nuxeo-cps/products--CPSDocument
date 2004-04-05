3ebb2e3fa47c3795205edbe55c91c57ed9b24b09
hcauwelier <devnull@localhost>
2004-04-05 18:08:06 +0000

    Fix bug where a non-editable content is modified. Thanks to Paolo Dina.

--- skins/cps_document/editLayouts.py
+++ skins/cps_document/editLayouts.py
@@ -23,18 +23,18 @@ for k in kw.keys():
         delete_rows.append(int(k[len('deleterow_'):]))
 
 if up_row is not None or down_row is not None:
-    context.getContent().flexibleChangeLayout(layout_id, up_row=up_row,
-                                              down_row=down_row)
+    context.getEditableContent().flexibleChangeLayout(layout_id, up_row=up_row,
+                                                      down_row=down_row)
     ret = 1
 
 if delete_rows:
-    context.getContent().flexibleDelWidgetRows(layout_id, delete_rows)
+    context.getEditableContent().flexibleDelWidgetRows(layout_id, delete_rows)
     ret = 1
 
 if kw.has_key('addwidget_button'):
     kwargs = {'label_edit': kw.get('widget_label_edit')}
-    context.getContent().flexibleAddWidget(layout_id, kw['widget_type'],
-                                           **kwargs)
+    context.getEditableContent().flexibleAddWidget(layout_id, kw['widget_type'],
+                                                   **kwargs)
     ret = 1
 
 return ret
