332fdacf962995a01d4bcc62af96d1b0fd0d3c42
afernandez <devnull@localhost>
2004-06-10 14:18:28 +0000

    cpsdocument_metadata.pt => cpsdocument_metadata.py if no error signaled by widget validation redirect to absolute_url of the document

---
+++ skins/cps_document/cpsdocument_metadata.py
@@ -0,0 +1,15 @@
+##parameters=REQUEST=None
+
+edit_metadata = context.portal_membership.checkPermission('Modify portal content', context)
+
+if not edit_metadata:
+    return context.cpsdocument_metadata_template(metadata=0)
+else:
+    rendered_main, portal_status_message = context.editCPSDocument(REQUEST=REQUEST,
+                                                                   layout_id='metadata')
+    error = portal_status_message == 'psm_content_error'
+    if error:
+        return context.cpsdocument_metadata_template(edit_metadata=1, rendered_main=rendered_main,
+                                                     portal_status_message=portal_status_message)
+    else:
+        REQUEST.RESPONSE.redirect(context.absolute_url())
