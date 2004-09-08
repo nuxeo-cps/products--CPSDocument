fc83ee5a0718856561d8236d9295990bced3cdea
janguenot <devnull@localhost>
2004-09-08 23:34:57 +0000

    Check CMF / CPS3

--- tests/CPSDocumentTestCase.py
+++ tests/CPSDocumentTestCase.py
@@ -1,12 +1,40 @@
 from Testing import ZopeTestCase
-from Products.CPSDefault.tests import CPSTestCase
 
-ZopeTestCase.installProduct('CPSDocument')
-ZopeTestCase.installProduct('CPSSchemas')
-ZopeTestCase.installProduct('PortalTransforms')
-ZopeTestCase.installProduct('Epoz')
+from Products.ExternalMethod.ExternalMethod import ExternalMethod
 
-CPSTestCase.setupPortal()
+try:
+    from Products.CPSDefault.tests import CPSTestCase
+    IS_CPS3 = 1
+except ImportError:
+    from Products.CPSDocument.tests import CMFTestCase
+    IS_CPS3 = 0
 
-CPSDocumentTestCase = CPSTestCase.CPSTestCase
+# CPS3 or stock CMF
+# Use a different installer class
+if IS_CPS3:
+    CPSTestCase.setupPortal()
+    CPSDocumentTestCase = CPSTestCase.CPSTestCase
+else:
+    class CPSDocumentInstaller(CMFTestCase.CMFInstaller):
+        def addPortal(self, id):
+            """Override the Default addPortal method installing
+            a Default CMF Site
 
+            Will launch the external method for CPSDocument
+            """
+
+            # CMF Default Site
+            CMFTestCase.CMFInstaller.addPortal(self, id)
+            portal = getattr(self.app, id)
+
+            # Install the CPSDocument product
+            if 'cpsdocument_installer' not in portal.objectIds():
+                cpsdocument_installer = ExternalMethod('cpsdocument_installer',
+                                                       '',
+                                                       'CPSDocument.install',
+                                                       'install')
+                portal._setObject('cpsdocument_installer', cpsdocument_installer)
+            portal.cpsdocument_installer()
+
+    CMFTestCase.setupPortal(PortalInstaller=CPSDocumentInstaller)
+    CPSDocumentTestCase = CMFTestCase.CMFTestCase
