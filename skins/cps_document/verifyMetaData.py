9d537d56e40fb6dc9b500403796aa6f19c5761f8
tziade <devnull@localhost>
2005-08-29 15:09:40 +0000

    reactivated effective date against creation date

--- skins/cps_document/verifyMetaData.py
+++ skins/cps_document/verifyMetaData.py
@@ -2,7 +2,6 @@
 # verifying dates integrity
 
 # $Id$
-
 import DateTime
 effective_date = datastructure['EffectiveDate']
 expiration_date = datastructure['ExpirationDate']
@@ -11,13 +10,13 @@ creation_date = datastructure['CreationDate']
 """
 XXXX need to fix effective_date algo that does not manage seconds
   ie : effective_date CAN be < to creation_date at 1 minute
-
+"""
 if effective_date is not None and effective_date <= (creation_date):
     datastructure.setError('EffectiveDate', 'cpsschemas_err_bad_date')
     return 0
-else
-"""
+
 if expiration_date is not None and expiration_date <= creation_date:
     datastructure.setError('ExpirationDate', 'cpsschemas_err_bad_date')
     return 0
+
 return 1
