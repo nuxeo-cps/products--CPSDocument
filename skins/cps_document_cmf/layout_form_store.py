5f4951f19e83148f9d6f92ec951bc04c319190ff
madarche <devnull@localhost>
2006-09-18 08:02:07 +0000

    - Removed the useless and unmaintained cps_document_cmf and cps_utils_cmf skins.

--- skins/cps_document_cmf/layout_form_store.py
+++
@@ -1,8 +0,0 @@
-##parameters=layout_mode, datastructure=None, **kw
-# $Id$
-"""Called when a form is validated by the user.
-"""
-
-#datamodel = datastructure.getDataModel()
-#raise 'ok', 'data is %s' % (datamodel,)
-pass
--- skins/cps_document_cmf/notifyCPSDocumentCreation.py
+++
@@ -1,7 +0,0 @@
-##parameters=ob
-# $Id$
-"""
-Hook for notifying when a cps document is created
-"""
-
-return None # Can't have empty python scripts. They don't compile. 
