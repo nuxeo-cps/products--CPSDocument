6eb11b2fd9550c86959149a2f6ccd35ee02b0b06
madarche <devnull@localhost>
2007-02-11 17:06:15 +0000

    - In verifyMetaData.py disabling the effective date check since it's possible to   create and publish documents after the effective date of the event. This   happens when one creates documents to cover past events.

--- skins/cps_document/verifyMetaData.py
+++ skins/cps_document/verifyMetaData.py
@@ -1,22 +1,27 @@
 ##parameters=datastructure, layout
-# verifying dates integrity
-
 # $Id$
+"""Verify metadata information.
+
+For now it only verify some dates.
+"""
+
 import DateTime
+
 effective_date = datastructure['EffectiveDate']
 expiration_date = datastructure['ExpirationDate']
 creation_date = datastructure['CreationDate']
 
-"""
-XXXX need to fix effective_date algo that does not manage seconds
-  ie : effective_date CAN be < to creation_date at 1 minute
-"""
-if effective_date is not None and effective_date <= (creation_date):
-    datastructure.setError('EffectiveDate', 'cpsschemas_err_bad_date')
-    return 0
+# Disabling this check since it's possible to create and publish documents after
+# the effective date of the event. This happens when one creates documents to
+# cover past events.
+#
+#if effective_date is not None and effective_date <= creation_date:
+#    datastructure.setError('EffectiveDate', 'cpsschemas_err_bad_date')
+#    return False
 
+# It is useless to create an already expired document
 if expiration_date is not None and expiration_date <= creation_date:
     datastructure.setError('ExpirationDate', 'cpsschemas_err_bad_date')
-    return 0
+    return False
 
-return 1
+return True
