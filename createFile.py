438e98bff52b68931d118840d2dfe8b8f7bbee71
Georges Racinet on Ishtar.racinet.fr <georges@racinet.fr>
2010-08-12 04:38:57 +0200

    #2208: automatic content creation (createFile now configurable)

--- createFile.py
+++ createFile.py
@@ -23,29 +23,70 @@ with types according to their extensions
 """
 
 from logging import getLogger
+from zipfile import ZipFile, BadZipfile
+from cStringIO import StringIO
+
+from zope.component import getAdapter
+from AccessControl import ModuleSecurityInfo
+from zExceptions import BadRequest
+from OFS.Image import Image, File
 
 from Products.CMFCore.utils import getToolByName
-from OFS.Image import File, Image
-from AccessControl import ModuleSecurityInfo
-from zipfile import ZipFile, BadZipfile
-from StringIO import StringIO
-from zExceptions import BadRequest
-from Products.CPSCore.EventServiceTool import getEventService
 from Products.CPSUtil.id import generateFileName
+from Products.CPSCore.EventServiceTool import getEventService
+from Products.CPSSchemas.BasicFields import CPSFileField, CPSImageField
 
 logger = getLogger('CPSDocument.createFile')
 
 ModuleSecurityInfo('Products.CPSDocument.createFile').declarePublic('createFile')
 
-def createFile(context, zip_file, check_allowed_content_types=True):
+class FileObjectFactory(object):
+    """A class that generate File objects, caching some info.
+
+    Must be transient.
+    """
+
+    # GR: this could be handled by a ZCA adapter,
+    # but I don't want to open this pandora box now; just opening CPSSchema's
+    # ZCML files is mind-blocking: two many things around
+    # (StorageAdapter, TramlineFile itself?) that could be done this way
+    # and clarified.
+    # Besides, OFS.Image.File has no interface anyway (!)
+    classes = { # field class -> file class
+        CPSFileField.meta_type: File,
+        CPSImageField.meta_type: Image,
+    }
+
+    def __init__(self, types_tool):
+        self.cache = {}
+        self.ttool = types_tool
+
+    def make(self, ptype, fid, oid, title, data):
+        fti = self.ttool[ptype]
+
+        # field retrieval with cache
+        fields_cache = self.cache
+        ckey = (ptype, fid)
+        field = fields_cache.get(ckey)
+        if field is None:
+            for s in fti._listSchemas(): # no flexible, of course
+                if not s.has_key(fid):
+                    continue
+                field = fields_cache[ckey] = s[fid]
+                break
+            else:
+                raise ValueError(
+                    "Field %s not in %s non-flexible schemas" % (fid, ptype))
+
+        # instantiation
+        klass = self.classes[field.meta_type]
+        return klass(oid, title, data)
+
+def createFile(container, zip_file, check_allowed_content_types=True):
     """create documents based on the files in the uploaded ZIP"""
     if zip_file is None:
         return
-    evtool = getEventService(context)
-    evtool.notifyEvent('modify_object', context, {})
-    registry = getToolByName(context, 'mimetypes_registry')
-    ttool = getToolByName(context, 'portal_types')
-    allowed_content_types = ttool[context.portal_type].allowed_content_types
+    evtool = getEventService(container)
 
     if hasattr(zip_file, 'filename'):
         filename = zip_file.filename
@@ -61,67 +102,51 @@ def createFile(context, zip_file, check_allowed_content_types=True):
             logger.info('Inexistent uploaded ZIP file')
             return
     try:
+        # TODO use OFSFileIO
         zipfile = ZipFile(StringIO(str(zip_file)))
     except BadZipfile:
         logger.info('Bad Zip File')
         return 0
-    infolist = zipfile.infolist()
-    if not check_allowed_content_types:
-        image_type_allowed = True
-    else:
-        image_type_allowed = 'Image' in  allowed_content_types
+
+    cont_ptype = getattr(container, 'portal_type', None)
+    if cont_ptype is None:
+        raise ValueError("Called for improper container %s" % container)
+
+    ttool = getToolByName(container, 'portal_types')
+    cont_fti = ttool[cont_ptype]
+
+    fobj_fact = FileObjectFactory(ttool)
+    proxy_fact = getToolByName(container, 'portal_workflow').invokeFactoryFor
+
+    evtool.notifyEvent('modify_object', container, {})
+
     # browsing the ZIP file
+    infolist = zipfile.infolist()
     for info in infolist:
         path = info.filename
         # Skip folders
         if path[-1] == '/':
             continue
+
         # Acquiring only the filename (without the directory path)
-        path_filename = generateFileName(path.split('/')[-1])
+        filename = path.rsplit('/', 1)[-1]
+        path_filename = generateFileName(filename)
 
-        mimetype = registry.lookupExtension(path_filename.lower())
-        if mimetype is not None:
-            mimetype = mimetype.normalized()
-        else:
-            mimetype = 'application/octet-stream'
-
-        if mimetype.startswith('image/') and image_type_allowed:
-            # use the Image portal type or fallback to File if Image is not
-            # allowed
-            ptype = 'Image'
-            field_name = 'preview'
-        else:
-            ptype = 'File'
-            field_name = 'file'
-        if check_allowed_content_types and ptype not in allowed_content_types:
+        ptype, fid = cont_fti.getAutoContentInfo(
+            file_name=path_filename, check_allowed=check_allowed_content_types)
+        if ptype is None:
             continue
+
+        fobj = fobj_fact.make(ptype, fid, path_filename, filename,
+                              zipfile.read(path))
         try:
-            file_id = context.portal_workflow.invokeFactoryFor(
-                context, ptype, path_filename)
+            file_id = proxy_fact(container, ptype, path_filename,
+                                 Title=filename, **{fid: fobj})
         except BadRequest:
             logger.info('File %s already exists', path_filename)
             return 0
 
-        file_proxy = getattr(context, file_id)
-        file_doc = file_proxy.getEditableContent()
-
-        # create file to attach to document
-        data = zipfile.read(path)
-        if ptype in ['Image']:
-            file_to_attach = Image(path_filename, path_filename, data)
-        else:
-            file_to_attach = File(path_filename, path_filename, data)
-        if mimetype and file_to_attach.content_type != mimetype:
-            logger.debug('Fixing mimetype from %s to %s',
-                         file_to_attach.content_type, mimetype)
-            file_to_attach.manage_changeProperties(content_type=mimetype)
-
-        doc_def = {
-            'Title': path_filename,
-            'Description': 'Imported File (original archive: %s)' % filename,
-            field_name: file_to_attach,
-        }
-
-        file_doc.edit(doc_def, proxy=file_proxy)
+        # there was a content_type correction of file obj in earlier version.
+        # might be necessary again (didn't see anything in history)
 
     return 1
