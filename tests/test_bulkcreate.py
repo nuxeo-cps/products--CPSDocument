7c340d27724e8aa49b148e0bca2a05a4852c21a0
Georges Racinet on purity.racinet.fr <georges@racinet.fr>
2010-11-26 02:55:26 +0100

    #2205: cut extension and use unique id generation

--- tests/test_bulkcreate.py
+++ tests/test_bulkcreate.py
@@ -23,38 +23,38 @@ class TestCreateFile(CPSTestCase):
         archive = self._makeZipFile()
         archive.name = "toto.zip"
         createFile(self.ws, archive, check_allowed_content_types=False)
-        self.assert_('endives.gif' in self.ws.objectIds())
-        self.assert_('felix.jpg' in self.ws.objectIds())
-        self.assert_('petit-chat.png' in self.ws.objectIds())
-        self.assert_('testCreateFile.py' in self.ws.objectIds())
+        self.assert_('endives' in self.ws.objectIds())
+        self.assert_('felix' in self.ws.objectIds())
+        self.assert_('petit-chat' in self.ws.objectIds())
+        self.assert_('testcreatefile' in self.ws.objectIds())
 
     def test_createFile_encoding_robustness(self):
         archive = self._makeZipFile('non-utf8.zip')
         archive.name = "non-utf8.zip"
         self.portal.default_charset = 'utf-8'
         createFile(self.ws, archive, check_allowed_content_types=False)
-        self.assert_('felin.jpg' in self.ws.objectIds())
+        self.assert_('felin' in self.ws.objectIds())
 
     def test_createFileWithFileName(self):
         archive = self._makeZipFile()
         archive.archivename = "toto.zip"
         createFile(self.ws, archive, check_allowed_content_types=False)
-        self.assert_('endives.gif' in self.ws.objectIds())
-        self.assert_('felix.jpg' in self.ws.objectIds())
-        self.assert_('petit-chat.png' in self.ws.objectIds())
-        self.assert_('testCreateFile.py' in self.ws.objectIds())
+        self.assert_('endives' in self.ws.objectIds())
+        self.assert_('felix' in self.ws.objectIds())
+        self.assert_('petit-chat' in self.ws.objectIds())
+        self.assert_('testcreatefile' in self.ws.objectIds())
 
     def test_detectPortalType(self):
         archive = self._makeZipFile()
         archive.name = "toto.zip"
         createFile(self.ws, archive, check_allowed_content_types=False)
-        image_ids = ('endives.gif', 'felix.jpg', 'petit-chat.png')
+        image_ids = ('endives', 'felix', 'petit-chat')
         for id in image_ids:
             image = getattr(self.ws, id)
             self.assertEquals(image.portal_type, 'Image')
             self.assert_(image.getContent()['preview'] is not None)
 
-        otherfile = getattr(self.ws, 'testCreateFile.py')
+        otherfile = getattr(self.ws, 'testcreatefile')
         self.assertEquals(otherfile.portal_type, 'File')
         self.assert_(otherfile.getContent()['file'] is not None)
 
@@ -64,14 +64,14 @@ class TestCreateFile(CPSTestCase):
         createFile(self.ws, archive, check_allowed_content_types=True)
         ws = self.ws
         # Worspace allows File but not Image
-        self.assert_(ws.hasObject('endives.gif'))
-        self.assert_(ws.hasObject('felix.jpg'))
-        self.assert_(ws.hasObject('petit-chat.png'))
-        self.assert_(ws.hasObject('testCreateFile.py'))
-        self.assertEquals(ws['endives.gif'].portal_type, 'File')
-        self.assertEquals(ws['felix.jpg'].portal_type, 'File')
-        self.assertEquals(ws['petit-chat.png'].portal_type, 'File')
-        self.assertEquals(ws['testCreateFile.py'].portal_type, 'File')
+        self.assert_(ws.hasObject('endives'))
+        self.assert_(ws.hasObject('felix'))
+        self.assert_(ws.hasObject('petit-chat'))
+        self.assert_(ws.hasObject('testcreatefile'))
+        self.assertEquals(ws['endives'].portal_type, 'File')
+        self.assertEquals(ws['felix'].portal_type, 'File')
+        self.assertEquals(ws['petit-chat'].portal_type, 'File')
+        self.assertEquals(ws['testcreatefile'].portal_type, 'File')
 
     def test_createFileWithCheckAllowedContentTypes2(self):
         archive = self._makeZipFile()
@@ -84,13 +84,13 @@ class TestCreateFile(CPSTestCase):
 
         # do not create file as sub objects of an image gallery
         createFile(ig, archive, check_allowed_content_types=True)
-        self.assert_(ig.hasObject('endives.gif'))
-        self.assert_(ig.hasObject('felix.jpg'))
-        self.assert_(ig.hasObject('petit-chat.png'))
-        self.failIf(ig.hasObject('testCreateFile.py'))
-        self.assertEquals(ig['endives.gif'].portal_type, 'Image')
-        self.assertEquals(ig['felix.jpg'].portal_type, 'Image')
-        self.assertEquals(ig['petit-chat.png'].portal_type, 'Image')
+        self.assert_(ig.hasObject('endives'))
+        self.assert_(ig.hasObject('felix'))
+        self.assert_(ig.hasObject('petit-chat'))
+        self.failIf(ig.hasObject('testcreatefile'))
+        self.assertEquals(ig['endives'].portal_type, 'Image')
+        self.assertEquals(ig['felix'].portal_type, 'Image')
+        self.assertEquals(ig['petit-chat'].portal_type, 'Image')
 
 
 def test_suite():
